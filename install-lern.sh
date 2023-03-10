d4bc4ec6fb833f87bdb992ad4119749fd1e6b750
Prasath Sivasubramaniyan <tsprasath91@gmail.com>
2023-03-10 19:25:50 +0530

    added cassandra

--- install-lern.sh
+++ install-lern.sh
@@ -46,3 +46,11 @@ for chart in */ ; do
     echo -e "\e[91mFailed to deploy ${chart_name}\e[0m"
   fi
 done
+
+
+
+#Install helm chart 3.7.1 for chart installation
+# curl -LO https://get.helm.sh/helm-v3.7.1-linux-amd64.tar.gz
+# tar -zxvf helm-v3.7.1-linux-amd64.tar.gz
+# sudo mv linux-amd64/helm /usr/local/bin/
+# helm version
